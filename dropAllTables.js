94871aaec3d2998671553effb8ee9699caf62d4c
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-11-24 16:43:13 -0500

    chore: create resetAndSeed script for deployment

---
+++ dropAllTables.js
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import { env } from "node:process";
+
+import { Pool } from "pg";
+
+const connectionString = env.DATABASE_URL;
+
+const pool = new Pool({
+  connectionString,
+});
+
+
+async function dropAllTables() {
+  const res = await pool.query(
+    "drop table if exists answer,character_name,scene,game,game_answer,session"
+  );
+  return res;
+}
+
+console.log("\nBegin table drop operation.\n");
+if (await dropAllTables()) {
+  console.log("All tables dropped.");
+  
+  await pool.end();
+}
---
+++ seedDB.js
@@ -0,0 +1,47 @@
+import "dotenv/config";
+import { env } from "node:process";
+
+import { Pool } from "pg";
+
+const connectionString = env.DATABASE_URL;
+
+const pool = new Pool({
+  connectionString,
+});
+
+async function seed() {
+  // set up the available scene
+  const res = await pool.query(
+    `insert into scene (url)
+    values ('https://res.cloudinary.com/hbrwdfccc/image/upload/v1763249346/Where%27s%20Waldo/Wheres-Waldo-Space-Station-Super-High-Resolution-scaled.jpg') returning (id);`
+  );
+
+  const sceneId = res.rows[0].id;
+
+  // setup the characters for that scene
+  const res1 =
+    res &&
+    (await pool.query(`insert into character_name (character, name, icon_url) 
+      values ('ODLAW', 'Odlaw', 'https://res.cloudinary.com/hbrwdfccc/image/upload/v1763875339/Where%27s%20Waldo/odlaw.png'),
+      ('WALDO', 'Waldo','https://res.cloudinary.com/hbrwdfccc/image/upload/v1763874393/Where%27s%20Waldo/wally.jpg'),
+      ('WIZARD_WHITEBEARD', 'Wizard Whitebeard','https://res.cloudinary.com/hbrwdfccc/image/upload/v1763875403/Where%27s%20Waldo/wizard.png') returning name`));
+
+  const res2 =
+    res1 &&
+    (await pool.query(
+      `insert into answer (scene_id, character, location_x, location_y) 
+      values (${sceneId}, 'ODLAW', 0.07, 24.82),
+      (${sceneId}, 'WALDO', 10.47, 25.12),
+      (${sceneId}, 'WIZARD_WHITEBEARD', 48.09, 20.6) returning scene_id`
+    ));
+
+  return res2;
+}
+
+console.log("\nBegin table seeding operation.\n");
+const res = await seed(); 
+
+if (res) {
+  console.log("All tables seeded successfully.");
+  await pool.end()
+}
